68d5fcf19d4fa87aba1c1d67135490c0a0062b05
aashi <aashijain.sangamone@gmail.com>
2022-03-09 01:50:26 +0530

    First Commit

---
+++ Assignment-1b.py
@@ -0,0 +1,8 @@
+n1=int(input("Enter start value:"))
+n2=int(input("Enter end value:"))
+for j in range(n1,n2+1,1):   
+    for i in range(1,11,1):
+        print(j,"*",i,"=",j*i)
+    print()
+        
+    
---
+++ Assignment-2a.py
@@ -0,0 +1,12 @@
+for j in range(3,0,-1):
+    for i in range(0,12,1):
+        angle=(j*30+i*30-i*2.5)
+        print(angle,angle%360)
+    print()
+
+for j in range(0,9,1):
+    for i in range(0,12,1):
+        angle=(j*30+i*30-i*2.5)
+        print(angle,angle%360)
+    print()
+
